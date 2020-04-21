bd8fc58c1447c02799382e224fe17a6df378f986
Tal-D <tdan.m92@gmail.com>
2020-04-21 23:41:50 +0300

    Moved producer main file constants values to constant variables (defined in the Main class)

--- kafka_producer_api/src/main/java/Main.java
+++ kafka_producer_api/src/main/java/Main.java
@@ -4,24 +4,26 @@ import producer.api.KafkaStringProducer;
 import utils.Constants;
 
 public class Main {
+    private static final String STRING_MESSAGE_CONTENT = "simple message";
+
+    private static final Identification IDENTIFICATION_AVRO_MESSAGE = Identification.newBuilder()
+            .setFirstName("William")
+            .setLastName("Foo-foo")
+            .setAge(42)
+            .setBirthCountry("UK")
+            .setPetName("Bunny")
+            .build();
+
     public static void main(String[] args) {
         KafkaStringProducer kafkaStringProducer = new KafkaStringProducer(Constants.BROKER,
                 Constants.SCHEMA_REGISTRY_URL);
         KafkaAvroProducer kafkaAvroProducer = new KafkaAvroProducer(Constants.BROKER,
                 Constants.SCHEMA_REGISTRY_URL);
 
-        String messageContent = "simple message";
-        kafkaStringProducer.sendMessage(Constants.SENT_MESSAGES_TOPIC, messageContent);
+        kafkaStringProducer.sendMessage(Constants.SENT_MESSAGES_TOPIC, STRING_MESSAGE_CONTENT);
         kafkaStringProducer.closeProducer();
 
-        Identification identification = Identification.newBuilder()
-                .setFirstName("William")
-                .setLastName("Foo-foo")
-                .setAge(42)
-                .setBirthCountry("UK")
-                .setPetName("Bunny")
-                .build();
-        kafkaAvroProducer.sendMessage(Constants.SENT_MESSAGES_TOPIC, identification);
+        kafkaAvroProducer.sendMessage(Constants.SENT_MESSAGES_TOPIC, IDENTIFICATION_AVRO_MESSAGE);
         kafkaAvroProducer.closeProducer();
     }
 }
