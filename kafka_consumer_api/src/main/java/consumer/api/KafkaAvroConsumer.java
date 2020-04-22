993d1269a00a316d38a853cec5241a3dd9bcb034
Tal-D <tdan.m92@gmail.com>
2020-04-22 20:43:57 +0300

    additional change to last commit (related change in kafka avro consumer class)

--- kafka_consumer_api/src/main/java/consumer/api/KafkaAvroConsumer.java
+++ kafka_consumer_api/src/main/java/consumer/api/KafkaAvroConsumer.java
@@ -1,10 +1,9 @@
 package consumer.api;
 
-import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import utils.Constants;
 
-public class KafkaAvroConsumer extends KafkaConsumer<SpecificRecord> {
+public class KafkaAvroConsumer<S> extends KafkaConsumer<S> {
     private String schemaRegistryUrl;
 
     public KafkaAvroConsumer(String broker, String schemaRegistryUrl) {
@@ -18,6 +17,6 @@ public class KafkaAvroConsumer extends KafkaConsumer<SpecificRecord> {
         super.initializeConsumer();
         this.consumerProperties.put(Constants.SCHEMA_REGISTRY_URL_COMPONENT_NAME, this.schemaRegistryUrl);
         this.consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, Constants.AVRO_DESERIALIZER_PATH);
-        this.consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<String, SpecificRecord>(this.consumerProperties);
+        this.consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<String, S>(this.consumerProperties);
     }
 }
