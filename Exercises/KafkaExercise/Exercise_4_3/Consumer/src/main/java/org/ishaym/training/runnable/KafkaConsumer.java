eaeb833865274dde92dfa194b1084e6070c3e99e
Ishai-Marom <maromish@gmail.com>
2020-04-26 18:55:24 +0300

    Ishai Marom - Kafka Exercise section 4.3
    section 4.3, contains fixes according to reviews on 4.1 and 4.2

--- Exercises/KafkaExercise/Exercise_4_3/Consumer/src/main/java/org/ishaym/training/runnable/KafkaConsumer.java
+++ Exercises/KafkaExercise/Exercise_4_3/Consumer/src/main/java/org/ishaym/training/runnable/KafkaConsumer.java
@@ -12,16 +12,16 @@ import java.io.IOException;
 import java.text.MessageFormat;
 import java.time.Duration;
 
-public class ConsumerAction implements Runnable {
-    private static final Logger LOGGER = LogManager.getLogger(ConsumerAction.class);
+public class KafkaConsumer implements Runnable {
+    private static final Logger LOGGER = LogManager.getLogger(KafkaConsumer.class);
 
     private final Consumer<Integer, Person> consumer;
 
-    public ConsumerAction(Consumer<Integer, Person> consumer) {
+    public KafkaConsumer(Consumer<Integer, Person> consumer) {
         this.consumer = consumer;
     }
 
-    private void action(ConsumerRecords<Integer, Person> records) {
+    private void logMessages(ConsumerRecords<Integer, Person> records) {
         for (ConsumerRecord<Integer, Person> record : records) {
             String output = MessageFormat.format("message key: {0} , message value: {1}",
                     record.key(), record.value());
@@ -35,8 +35,9 @@ public class ConsumerAction implements Runnable {
 
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                action(consumer.poll(Duration.ofMillis(Constants.genInstance().getConfigurations().
-                        getConsumerProperties().getPollingTimeoutInMilliSeconds())));
+                logMessages(consumer.poll(Duration.ofMillis(Constants.genInstance().
+                        getConfigurations().getConsumerProperties().
+                        getPollingTimeoutInMilliSeconds())));
             } catch (IOException e) {
                 LOGGER.fatal(e);
                 System.exit(-1);
--- Exercises/KafkaExercise/Exercise_4_3/Producer/src/main/java/org/ishaym/training/common/Constants.java
+++ Exercises/KafkaExercise/Exercise_4_3/Producer/src/main/java/org/ishaym/training/common/Constants.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.ishaym.training.config.Configurations;
+import org.ishaym.training.defaults.DefaultMessage;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,23 +17,27 @@ public class Constants {
     private static Constants constants = null;
 
     private static final String PROPERTIES_FILE = "configurations.yaml";
+    private static final String DEFAULT_MESSAGE_FILE = "default_message.yaml";
 
     private Configurations configurations;
+    private DefaultMessage defaultMessage;
 
-    private Configurations getConfigurationsFromFile() throws IOException {
+
+    private <T> T getDataFromYAMLFile(String fileName, Class<T> valueType) throws IOException {
         LOGGER.debug("started reading properties from the yaml file");
 
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         File file = new File(Objects.requireNonNull(
-                classLoader.getResource(PROPERTIES_FILE)).getFile());
+                classLoader.getResource(fileName)).getFile());
         ObjectMapper om = new ObjectMapper(new YAMLFactory());
-        return om.readValue(file, Configurations.class);
+        return om.readValue(file, valueType);
     }
 
     private Constants() throws IOException {
         LOGGER.debug("creating the constants object instance");
 
-        this.configurations = getConfigurationsFromFile();
+        this.configurations = getDataFromYAMLFile(PROPERTIES_FILE, Configurations.class);
+        this.defaultMessage = getDataFromYAMLFile(DEFAULT_MESSAGE_FILE, DefaultMessage.class);
     }
 
     public static Constants genInstance() throws IOException {
@@ -47,4 +52,9 @@ public class Constants {
     public Configurations getConfigurations() {
         return configurations;
     }
+
+    public DefaultMessage getDefaultMessage() {
+        return defaultMessage;
+    }
+
 }
