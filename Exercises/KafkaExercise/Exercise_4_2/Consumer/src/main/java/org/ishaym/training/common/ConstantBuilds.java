93657a355c951bc84e910690672c8d6aab5a6b3f
Ishai-Marom <maromish@gmail.com>
2020-04-30 18:52:35 +0300

    Ishai Marom - Kafka Exercise section 4.2
    section 4.2, fixed according to review on section 4.3.

---
+++ Exercises/KafkaExercise/Exercise_4_2/Consumer/src/main/java/org/ishaym/training/common/ConstantBuilds.java
@@ -0,0 +1,62 @@
+package org.ishaym.training.common;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.ishaym.training.Person;
+import org.ishaym.training.config.ConsumerProperties;
+import org.ishaym.training.config.KafkaProperties;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConstantBuilds {
+    private static final Logger LOGGER = LogManager.getLogger(ConstantBuilds.class);
+
+    private ConstantBuilds() {
+
+    }
+
+    private static Properties createAdminClientPropertiesObject() throws IOException {
+        LOGGER.debug("started creating the admin properties object");
+
+        KafkaProperties kafkaProperties = Constants.getKafkaProperties();
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", kafkaProperties.getBootstrapServer());
+        properties.put("client.id", kafkaProperties.getClientId());
+
+        return properties;
+    }
+
+    private static Properties createConsumerPropertiesObject() throws IOException {
+        LOGGER.debug("started creating the consumer properties object");
+
+        KafkaProperties kafkaProperties = Constants.getKafkaProperties();
+        ConsumerProperties consumerProperties = Constants.getConsumerProperties();
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", kafkaProperties.getBootstrapServer());
+        properties.put("schema.registry.url", kafkaProperties.getSchemaRegistryUrl());
+        properties.put("group.id", consumerProperties.getGroupId());
+        properties.put("key.deserializer", consumerProperties.getKeyDeserializer());
+        properties.put("value.deserializer", consumerProperties.getValueDeserializer());
+        properties.put("specific.avro.reader", consumerProperties.isSpecificAvroReader());
+
+        return properties;
+    }
+
+    public static AdminClient buildAdminClientFromConstants() throws IOException {
+        LOGGER.debug("building the admin client from the properties");
+
+        return AdminClient.create(createAdminClientPropertiesObject());
+    }
+
+    public static Consumer<Integer, Person> buildConsumerFromConstants() throws IOException {
+        LOGGER.debug("building the Kafka Consumer from the properties");
+
+        return new KafkaConsumer<>(createConsumerPropertiesObject());
+    }
+}
